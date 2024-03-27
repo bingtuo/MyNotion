78943b33da058979bc7b0906fe131de112ab6e9b
冰拓 <38130106+bingtuo@users.noreply.github.com>
2024-03-27 23:51:25 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -13,6 +13,7 @@ const Footer = ({ title }) => {
       {/* <DarkModeButton/> */}
 
       <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={siteConfig('LINK')} className='underline font-bold  dark:text-gray-300 '>{siteConfig('AUTHOR')}</a>.<br/>
+      <i className='fas fa-shield-alt'/><a href="https://icp.gov.moe/?keyword=20240801" target="_blank">萌ICP备20240801号</a>
 
       {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a><br/></>}
 
@@ -20,8 +21,7 @@ const Footer = ({ title }) => {
             <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
       <span className='pl-2 hidden busuanzi_container_site_uv'>
         <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-        <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1>
-        <a href="https://icp.gov.moe/?keyword=20240801" target="_blank">萌ICP备20240801号</a></span><br/>
+        <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1></span><br/>
 
     </footer>
   )
