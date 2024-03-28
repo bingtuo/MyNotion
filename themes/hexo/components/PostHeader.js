5b0566612ca1cede143a400e0cf81cdb52f2f6b5
冰拓 <38130106+bingtuo@users.noreply.github.com>
2024-03-28 15:04:36 +0800

    Update PostHeader.js

--- themes/hexo/components/PostHeader.js
+++ themes/hexo/components/PostHeader.js
@@ -41,36 +41,26 @@ export default function PostHeader({ post, siteInfo }) {
             </div>
 
          {/* 文章Title */}
-          <div className='leading-snug font-bold xs:text-4xl sm:text-4xl md:text-5xl md:leading-snug text-4xl shadow-text-md flex justify-center text-center text-white'>
-            <NotionIcon icon={post.pageIcon} className='text-4xl mx-1' />
-            {post.title}
+          <div className="leading-snug font-bold xs:text-4xl sm:text-4xl md:text-5xl md:leading-snug text-4xl shadow-text-md flex justify-center text-center text-white">
+            <NotionIcon icon={post.pageIcon} className='text-4xl mx-1' />{post.title}
           </div>
-
-          <section className='flex-wrap shadow-text-md flex text-sm justify-center mt-4 text-white dark:text-gray-400 font-light leading-8'>
+          <section className="flex-wrap shadow-text-md flex text-sm justify-center mt-4 text-white dark:text-gray-400 font-light leading-8">
             <div className='flex justify-center dark:text-gray-200 text-opacity-70'>
+              <div className='mr-2'><WordCount /></div>
               {post?.type !== 'Page' && (
                 <>
-                  <>
-                    {post?.publishDay && (
-                      <span className="mx-3">
-                        <i className="fas fa-hourglass-half pr-2"></i>
-                        <span>{locale.COMMON.POST_TIME}: {post.publishDay}</span>
-                      </span>
-                    )}
-                  </>
+                  <Link
+                    href={`/archive#${formatDateFmt(post?.publishDate, 'yyyy-MM')}`}
+                    passHref
+                    className="pl-1 mr-2 cursor-pointer hover:underline">
+                    {locale.COMMON.POST_TIME}: {post?.publishDay}
+                  </Link>
                 </>
               )}
-              {/* 最后更新 */}
-              {post?.finished_date && post?.finished_date !== 'Invalid Date' && (
-                <div className='mx-4'>
-                  <i className="far fa-calendar-check pr-2"></i>
-                  {locale.COMMON.LAST_EDITED_TIME}: {post.finished_date}
-                </div>
-              )}
+              <div className="pl-1 mr-2">
+                {locale.COMMON.LAST_EDITED_TIME}: {post.lastEditedDay}
+              </div>
             </div>
-
-            {/* 文章字数 */}
-            <span className='mx-2'><WordCount /></span>
 
             {/* 次查看 */}
             {/*{JSON.parse(siteConfig('ANALYTICS_BUSUANZI_ENABLE')) && <div className="busuanzi_container_page_pv font-light mr-2">
