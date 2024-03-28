34e490adf9c0ee3400654d0ce200ba67cba00f6c
冰拓 <38130106+bingtuo@users.noreply.github.com>
2024-03-28 14:27:11 +0800

    Update WordCount.js

--- components/WordCount.js
+++ components/WordCount.js
@@ -15,8 +15,7 @@ export default function WordCount() {
         <span className='flex whitespace-nowrap items-center'>
           <span className='mr-2'>
             <i className='pl-1 pr-2 fas fa-file-word' />
-            <span>{locale.COMMON.WORD_COUNT}</span>&nbsp;
-            <span id='wordCount'>0</span>
+            {locale.COMMON.WORD_COUNT}: <span id='wordCount'>0</span>
           </span>
         </span>
         <span className='flex whitespace-nowrap items-center'>
