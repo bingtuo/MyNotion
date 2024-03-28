c7f89acb8f881ffd73223cb0c559a3fd43a3a888
冰拓 <38130106+bingtuo@users.noreply.github.com>
2024-03-28 10:24:23 +0800

    页脚鱼

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -19,7 +19,9 @@ const Footer = ({ title }) => {
       <span className='pl-2 hidden busuanzi_container_site_uv'>
         <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
         <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1></span><br/>
+        <Fish/>
     </footer>
+                                                                                
   )
 }
 export default Footer
