b7e837b657a8a07ad9626af8a2a06528c1503073
冰拓 <38130106+bingtuo@users.noreply.github.com>
2024-03-28 14:03:23 +0800

    Update PostHeader.js

--- themes/hexo/components/PostHeader.js
+++ themes/hexo/components/PostHeader.js
@@ -64,11 +64,10 @@ export default function PostHeader({ post, siteInfo }) {
               </div>
             </div>
 
-            {/* 次查看 */}
-            {/*{JSON.parse(siteConfig('ANALYTICS_BUSUANZI_ENABLE')) && <div className="busuanzi_container_page_pv font-light mr-2">
+            {JSON.parse(siteConfig('ANALYTICS_BUSUANZI_ENABLE')) && <div className="busuanzi_container_page_pv font-light mr-2">
               <span className="mr-2 busuanzi_value_page_pv" />
               {locale.COMMON.VIEWS}
-            </div>}*/}
+            </div>}
           </section>
 
             <div className='mt-4 mb-1'>
