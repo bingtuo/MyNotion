6013fdec4ae9c35e05a961085b320dcd0bca08d8
冰拓 <38130106+bingtuo@users.noreply.github.com>
2025-09-17 20:00:03 +0800

    Update SEO.js 添加bing搜索引擎seo

--- components/SEO.js
+++ components/SEO.js
@@ -71,6 +71,12 @@ const SEO = props => {
     NOTION_CONFIG
   )
 
+  const SEO_BING_SITE_VERIFICATION = siteConfig(
+    'SEO_BING_SITE_VERIFICATION',
+    null,
+    NOTION_CONFIG
+  )
+
   const BLOG_FAVICON = siteConfig('BLOG_FAVICON', null, NOTION_CONFIG)
 
   const COMMENT_WEBMENTION_ENABLE = siteConfig(
@@ -128,6 +134,12 @@ const SEO = props => {
           content={SEO_BAIDU_SITE_VERIFICATION}
         />
       )}
+      {SEO_BING_SITE_VERIFICATION && (
+        <meta
+          name='msvalidate.01'
+          content={SEO_BING_SITE_VERIFICATION}
+        />
+      )}  
 
       {/* 基础SEO元数据 */}
       <meta name='keywords' content={keywords} />
